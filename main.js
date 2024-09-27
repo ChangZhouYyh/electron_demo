2114779f9dd496e856ba1361f84e00ced17a026f
ChangZhouYyh <528471661@qq.com>
2024-09-27 17:21:42 +0800

    更新文件
    安装electron-builder 和 electron-updater

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Modules to control application life and create native browser window
+// 控制应用程序生命周期和创建本地浏览器窗口的模块
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 function createWindow () {
-  // Create the browser window.
+  // 创建浏览器窗口。
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,32 +12,28 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
+  // 加载应用的 index.html。
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
+  // 打开开发者工具。
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// 当 Electron 完成初始化并准备创建浏览器窗口时，将调用此方法。
+// 一些 API 只能在此事件发生后使用。
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // 在 macOS 上，通常在应用的 dock 图标被点击且没有其他窗口打开时重新创建窗口。
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// 当所有窗口关闭时退出，除了 macOS。在 macOS 上，应用程序及其菜单栏通常保持活动状态，直到用户显式使用 Cmd + Q 退出。
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// 在此文件中，您可以包含应用特定的主进程代码。
+// 您也可以将它们放在单独的文件中并在此处引入。
