9d09c4176d1b76af6db3aaf701e99e1f7ff731fa
ChangZhouYyh <528471661@qq.com>
2024-09-29 09:21:51 +0800

    设置打包程序

--- main.js
+++ main.js
@@ -1,8 +1,55 @@
 // 控制应用程序生命周期和创建本地浏览器窗口的模块
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const path = require('node:path')
 
-function createWindow () {
+
+
+/** 应用最新版本查询 */
+function checkUpdate() {
+
+
+  autoUpdater.setFeedURL({
+    provider: "github",
+    owner: "ChangZhouYyh",
+    repo: "electron_demo",
+    token: process.env.GITHUB_TOKEN
+  });
+
+
+  // 检测更新
+  autoUpdater.checkForUpdates();
+
+  //监听'error'事件
+  autoUpdater.on('error', (err) => {
+    console.log(err);
+  })
+
+  //监听'update-available'事件，发现有新版本时触发
+  autoUpdater.on('update-available', () => {
+    console.log('found new version');
+  })
+
+  //默认会自动下载新版本，如果不想自动下载，设置autoUpdater.autoDownload = false
+
+  //监听'update-downloaded'事件，新版本下载完成时触发
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用更新',
+      message: '发现新版本，是否更新？',
+      buttons: ['是', '否']
+    }).then((buttonIndex) => {
+      if (buttonIndex.response == 0) {
+        autoUpdater.quitAndInstall();
+        app.quit();
+      }
+    })
+  })
+}
+
+
+function createWindow() {
   // 创建浏览器窗口。
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -22,6 +69,7 @@ function createWindow () {
 // 当 Electron 完成初始化并准备创建浏览器窗口时，将调用此方法。
 // 一些 API 只能在此事件发生后使用。
 app.whenReady().then(() => {
+  checkUpdate()
   createWindow()
 
   app.on('activate', function () {
